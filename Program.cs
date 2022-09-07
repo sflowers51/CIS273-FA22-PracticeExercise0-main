43f7f6809c9f69080f2b8d882fa2758237dadd67
sflowers51 <111446369+sflowers51@users.noreply.github.com>
2022-09-06 23:29:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace PracticeExcercise0
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IsPalindrome Method");
+            Console.WriteLine(IsPalindrome("mom "));
+            Console.WriteLine(IsPalindrome("Mom"));
+            Console.WriteLine(IsPalindrome("Race car"));
+            Console.WriteLine(IsPalindrome("A nut for a jar of tuna"));
+            Console.WriteLine("");
+
+            Console.WriteLine("Reverse string Method");
+            Console.WriteLine(Reverse("coffee"));
+            Console.WriteLine(Reverse("abcdefghijklmnopqrstuvwxyz"));
+            Console.WriteLine(Reverse("Dad"));
+            Console.WriteLine(Reverse(""));
+            Console.WriteLine(Reverse("savannaH"));
+            Console.WriteLine("");
+
+            Console.WriteLine("Reverse int method");
+            Console.WriteLine(ReverseInt(369));  
+            Console.WriteLine(ReverseInt(-369));  
+            Console.WriteLine("");
+
+            Console.WriteLine("IsUnique Method");
+            Console.WriteLine(IsUnique("mom"));
+            IsUnique("");
+            IsUnique("mom");
+            IsUnique("Robbie");
+            IsUnique("Judah");
+            Console.WriteLine("");
+
+        }
+
+
+        // #1
+        public static bool IsPalindrome(string s)
+        {
+            // clean up the string
+            // remove spaces
+            // lowercase it
+            string cleanString = s.ToLower().Replace(" ", "");
+
+            // reverse the clean string
+            string reversed = Reverse(cleanString);
+
+            // check if reverse string is the same as the clean string
+
+          
+            return (reversed == cleanString);
+        }
+
+        private static string Reverse(string s)
+        {
+            string result = "";
+
+            foreach( char c in s)
+            {
+                result = result.Insert(0, c.ToString() );
+            }
+
+            return result;
+        }
+
+        // #2 
+        public static int ReverseInt(int i)
+        {
+            int x = i;
+            int reverse = 0;
+
+            while( x != 0 )
+            {
+                int remainder = x % 10;
+                reverse = reverse * 10 + remainder;
+                x = x / 10;
+            }
+
+            return reverse;
+        }
+
+        // #3
+        public static bool IsUnique(string s)
+        {
+            string cleanString = s.ToLower().Replace(" ", "");
+
+            for( int i=0; i < cleanString.Length; i++)
+            {
+                // compare cleanString[i] to every subsequent letter
+                // in cleanString
+
+                for(int j=i+1; j < cleanString.Length; j++)
+                {
+                    if (cleanString[i] == cleanString[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+
+        // #4
+        public static bool IsNeilNumber(int i)
+        {
+            return true;
+        }
+
+    }
+}
